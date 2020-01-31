c37f4abc928741ffb4c0ce7b3593d10ff1319663
Brett Bornhoft <brettbornhoft@DNab4075d0.stanford.edu>
2020-01-30 16:25:23 -0800

    bug fix inviscid burgers test case

--- Cases/Scalar_1D/BurgersEquation/invBurgers.py
+++ Cases/Scalar_1D/BurgersEquation/invBurgers.py
@@ -11,7 +11,7 @@ import Limiter
 ### Mesh
 Periodic = False
 # Uniform mesh
-mesh = MeshCommon.Mesh1D(Uniform=True, nElem=100, xmin=-1., xmax=1., Periodic=Periodic)
+mesh = MeshCommon.Mesh1D(Uniform=True, nElem=50, xmin=0., xmax=1., Periodic=Periodic)
 # Non-uniform mesh
 # nElem = 25
 # Coords = np.cos(np.linspace(np.pi,0.,nElem+1))
@@ -21,12 +21,15 @@ mesh = MeshCommon.Mesh1D(Uniform=True, nElem=100, xmin=-1., xmax=1., Periodic=Pe
 
 
 ### Solver parameters
-EndTime = 0.4
-nTimeStep = np.amax([1,int(EndTime/((mesh.Coords[1,0] - mesh.Coords[0,0])*0.1))])
-#nTimeStep = 1
+dt = 0.001
+mu = 1.
+EndTime = 0.3
+#nTimeStep = np.amax([1,int(EndTime/((mesh.Coords[1,0] - mesh.Coords[0,0])*0.1))])
+nTimeStep = int(EndTime/dt)
 InterpOrder = 2
 Params = General.SetSolverParams(InterpOrder=InterpOrder,EndTime=EndTime,nTimeStep=nTimeStep,
-								 InterpBasis="SegLagrange",TimeScheme="RK4")
+								 InterpBasis="SegLagrange",TimeScheme="SSPRK3",
+								 ApplyLimiter="ScalarPositivityPreserving")
 
 ### Physics
 ConstVelocity = 1.
@@ -35,9 +38,9 @@ EqnSet.SetParams(AdvectionOperator="Burgers")
 EqnSet.SetParams(ConstVelocity=ConstVelocity, ConvFlux="LaxFriedrichs")
 
 # Initial conditions
-EqnSet.IC.Set(Function=EqnSet.FcnScalarShock, minVal = 1., maxVal = 2.,  xshock = -0.5)
+EqnSet.IC.Set(Function=EqnSet.FcnScalarShock, uL = 1., uR = 0.,  xshock = 0.3)
 # Exact solution
-EqnSet.ExactSoln.Set(Function=EqnSet.FcnScalarShock, minVal = 1., maxVal = 2., xshock = -0.5)
+EqnSet.ExactSoln.Set(Function=EqnSet.FcnScalarShock, uL = 1., uR = 0., xshock = 0.3)
 # Boundary conditions
 if ConstVelocity >= 0.:
 	Inflow = "Left"; Outflow = "Right"
@@ -48,7 +51,7 @@ if not Periodic:
 		BC = EqnSet.BCs[ibfgrp]
 		## Left
 		if BC.Name is Inflow:
-			BC.Set(Function=EqnSet.FcnScalarShock, BCType=EqnSet.BCType["FullState"], minVal = 1., maxVal = 2., xshock = -0.5)
+			BC.Set(Function=EqnSet.FcnScalarShock, BCType=EqnSet.BCType["FullState"], uL = 1., uR = 0., xshock = 0.3)
 		elif BC.Name is Outflow:
 			BC.Set(BCType=EqnSet.BCType["Extrapolation"])
 			# BC.Set(Function=EqnSet.FcnSine, BCType=EqnSet.BCType["FullState"], omega = 2*np.pi)
@@ -66,7 +69,7 @@ solver.solve()
 TotErr,_ = Post.L2_error(mesh, EqnSet, solver.Time, "Scalar")
 # Plot
 Plot.PreparePlot()
-Plot.PlotSolution(mesh, EqnSet, solver.Time, "Scalar", PlotExact = True, Label="Q_h")
+Plot.PlotSolution(mesh, EqnSet, solver.Time, "Scalar", PlotExact = True, Label="u")
 Plot.ShowPlot()
 
 
