dac4293ca28aab31428796be52cec40956470519
Brett Bornhoft <bbornhoft@gmail.com>
2020-04-01 14:33:36 -0700

    Add print statements to TestAllCases

--- TestAllCases.py
+++ TestAllCases.py
@@ -83,16 +83,19 @@ if args.test_1D is True:
 	sys.path.append('Cases/Euler_1D/SmoothIsentropicFlow')
 	import SmoothIsentropicFlow
 	assert_almost_equal(SmoothIsentropicFlow.TotErr, 0.000682718239352, decimal=decimal)
+	print('Pass 1D Smooth Isentropic Flow [Euler 1D]')
 
 	# Moving shock
 	sys.path.append('Cases/Euler_1D/MovingShock')
 	import MovingShock
 	assert_almost_equal(MovingShock.TotErr, 0.172768793744101, decimal=decimal)
+	print('Pass 1D Moving Shock [Euler 1D]')
 
 	if args.ader is True:
 		# Smooth isentropic flow ADER
 		import ADER_SmoothIsentropicFlow
 		assert_almost_equal(ADER_SmoothIsentropicFlow.TotErr, 0.000658149093854, decimal=decimal)
+		print('Pass 1D ADER Smooth Isentropic Flow [Euler 1D]')
 
 if args.test_2D is True:
 
@@ -104,11 +107,14 @@ if args.test_2D is True:
 	sys.path.append('Cases/Euler_2D/VortexPropagation')
 	import VortexPropagation
 	assert_almost_equal(VortexPropagation.TotErr, 0.009663006404448, decimal=decimal)
+	print('Pass 2D Vortex Propagation')
+
 
 	# Flow over bump
 	sys.path.append('Cases/Euler_2D/FlowOverBump')
 	import FlowOverBump
 	assert_almost_equal(FlowOverBump.TotErr, 0.000012270459601, decimal=decimal)
+	print('Pass 2D Flow Over Bump')
 
 
 
