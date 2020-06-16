f5938d148f2c41a4f3933aa35a5abcec3d3ac41c
Brett Bornhoft <bbornhoft@gmail.com>
2020-06-16 11:29:42 -0700

    Add DampingSineWave example case
    - Add write out to PDF for example cases.

---
+++ examples/scalar/1D/DampingSineWave/DampingSineWave.py
@@ -0,0 +1,60 @@
+import numpy as np
+
+TimeStepping = {
+    "StartTime" : 0.,
+    "EndTime" : 0.5,
+    "nTimeStep" : 40,
+    "TimeScheme" : "RK4",
+}
+
+Numerics = {
+    "InterpOrder" : 2,
+    "InterpBasis" : "LagrangeEqSeg",
+    "Solver" : "DG",
+}
+
+Output = {}
+
+Mesh = {
+    "File" : None,
+    "ElementShape" : "Segment",
+    "nElem_x" : 16,
+    "nElem_y" : 2,
+    "xmin" : -1.,
+    "xmax" : 1.,
+    # "PeriodicBoundariesX" : ["x1", "x2"],
+}
+
+Physics = {
+    "Type" : "ConstAdvScalar",
+    "ConvFlux" : "LaxFriedrichs",
+    "ConstVelocity" : 1.,
+}
+
+nu = -3.
+InitialCondition = {
+    "Function" : "DampingSine",
+    "omega" : 2*np.pi,
+    "nu" : nu,
+    "SetAsExact" : True,
+}
+
+BoundaryConditions = {
+    "Left" : {
+	    "Function" : "DampingSine",
+	    "omega" : 2*np.pi,
+	    "nu" : nu,
+    	"BCType" : "FullState",
+    },
+    "Right" : {
+    	"Function" : None,
+    	"BCType" : "Extrapolation",
+    },
+}
+
+SourceTerms = {
+	"source1" : {
+		"Function" : "SimpleSource",
+		"nu" : nu,
+	},
+}
