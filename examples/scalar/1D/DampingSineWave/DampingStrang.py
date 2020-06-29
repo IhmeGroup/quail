4afe9c1f5c5f6823fcaf309bdb33bc42fed290c7
Brett Bornhoft <bbornhoft@gmail.com>
2020-06-29 16:21:02 -0700

    Test case for strang

---
+++ examples/scalar/1D/DampingSineWave/DampingStrang.py
@@ -0,0 +1,71 @@
+import numpy as np
+cfl = .1
+dx = 0.125
+dt = cfl*dx
+EndTime = 0.5
+nTimeStep = int(EndTime/dt)
+TimeStepping = {
+    "StartTime" : 0.,
+    "EndTime" : EndTime,
+    "nTimeStep" : nTimeStep,
+    "TimeScheme" : "Strang",
+}
+
+Numerics = {
+    "InterpOrder" : 2,
+    # "InterpolateIC" : True,
+    "InterpBasis" : "LagrangeEqSeg",
+    "Solver" : "DG",
+    "ConvFluxSwitch" : True,
+    "SourceTreatment" : "Implicit"
+}
+
+Output = {
+    "AutoProcess" : True
+}
+
+Mesh = {
+    "File" : None,
+    "ElementShape" : "Segment",
+    "nElem_x" : 16,
+    "nElem_y" : 2,
+    "xmin" : -1.,
+    "xmax" : 1.,
+    "PeriodicBoundariesX" : ["x1", "x2"],
+}
+
+Physics = {
+    "Type" : "ConstAdvScalar",
+    "ConvFlux" : "LaxFriedrichs",
+    "ConstVelocity" : 1.,
+}
+
+nu = -10000.
+InitialCondition = {
+    "Function" : "DampingSine",
+    "omega" : 2*np.pi,
+    "nu" : nu ,
+    # "state" : [1.0],
+}
+
+ExactSolution = InitialCondition.copy()
+
+BoundaryConditions = {
+    # "Left" : {
+	   #  "Function" : "DampingSine",
+	   #  "omega" : 2*np.pi,
+	   #  "nu" : nu,
+    # 	"BCType" : "StateAll",
+    # },
+    # "Right" : {
+    # 	#"Function" : None,
+    # 	"BCType" : "Extrapolate",
+    # },
+}
+
+SourceTerms = {
+	"source1" : {
+		"Function" : "SimpleSource",
+		"nu" : nu,
+	},
+}
