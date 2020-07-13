249762846368c4e50df65338da08f19f1689cf4c
Brett Bornhoft <bbornhoft@gmail.com>
2020-07-13 11:12:37 -0700

    Add simple chemistry physics class

---
+++ src/physics/chemistry/chemistry.py
@@ -0,0 +1,254 @@
+import code
+from enum import Enum
+import numpy as np
+from scipy.optimize import fsolve, root
+
+import errors
+import general
+
+import physics.base.base as base
+import physics.euler.euler as euler
+import physics.base.functions as base_fcns
+from physics.base.functions import BCType as base_BC_type
+from physics.base.functions import ConvNumFluxType as base_conv_num_flux_type
+from physics.base.functions import FcnType as base_fcn_type
+
+import physics.euler.functions as euler_fcns
+from physics.euler.functions import BCType as euler_BC_type
+from physics.euler.functions import ConvNumFluxType as euler_conv_num_flux_type
+from physics.euler.functions import FcnType as euler_fcn_type
+from physics.euler.functions import SourceType as euler_source_type
+
+import physics.chemistry.functions as chemistry_fcns
+from physics.chemistry.functions import FcnType as chemistry_fcn_type
+from physics.chemistry.functions import SourceType as chemistry_source_type
+
+
+class Chemistry(base.PhysicsBase):
+
+	PHYSICS_TYPE = general.PhysicsType.Chemistry
+
+	def __init__(self, order, basis, mesh):
+		'''
+		Method: __init__
+		--------------------------------------------------------------------------
+		This method initializes the temperature table. The table uses a
+		piecewise linear function for the constant pressure specific heat 
+		coefficients. The coefficients are selected to retain the exact 
+		enthalpies at the table points.
+		'''
+		super().__init__(order, basis, mesh)
+		# Default parameters
+		self.Params.update(
+			GasConstant = 287., # specific gas constant
+			SpecificHeatRatio = 1.4,
+			HeatRelease = 25. 
+		)
+
+	# def set_maps(self):
+	# 	super().set_maps()
+
+		self.BC_map.update({
+			euler_BC_type.SlipWall : euler_fcns.SlipWall,
+		# 	euler_BC_type.PressureOutlet : euler_fcns.PressureOutlet,
+		})
+	def set_physical_params(self, GasConstant=287., SpecificHeatRatio = 1.4, HeatRelease = 0.):
+		self.R = GasConstant
+		self.gamma = SpecificHeatRatio
+		self.qo = HeatRelease
+
+	class AdditionalVariables(Enum):
+	    Pressure = "p"
+	    Temperature = "T"
+	    Entropy = "s"
+	    InternalEnergy = "\\rho e"
+	    TotalEnthalpy = "H"
+	    SoundSpeed = "c"
+	    MaxWaveSpeed = "\\lambda"
+	    Velocity = "u"
+	    MassFraction = "Y"
+	    SourceTerm = "S"
+	    Jacobian = "J"
+
+	def ConvFluxInterior(self, Up):
+		dim = self.dim
+		
+		irho = self.GetStateIndex("Density")
+		irhoE = self.GetStateIndex("Energy")
+		irhoY = self.GetStateIndex("Mixture")
+		# imom = self.GetMomentumSlice()
+		srho = self.get_state_slice("Density")
+		srhoE = self.get_state_slice("Energy")
+		smom = self.GetMomentumSlice()
+		srhoY = self.get_state_slice("Mixture")
+
+		eps = general.eps
+
+		rho = Up[:, srho]
+		rho += eps
+		rhoE = Up[:,srhoE]
+		mom = Up[:,smom]
+		rhoY = Up[:,srhoY]
+
+		p = self.ComputeScalars("Pressure", Up)
+		h = self.ComputeScalars("TotalEnthalpy", Up)
+
+		pmat = np.zeros([Up.shape[0], dim, dim])
+		idx = np.full([dim,dim],False)
+		np.fill_diagonal(idx,True)
+		pmat[:, idx] = p
+
+		F = np.empty(Up.shape + (dim,))
+		F[:, irho, :] = mom
+		F[:, smom, :] = np.einsum('ij,ik->ijk',mom,mom)/np.expand_dims(rho, axis=2) + pmat
+		F[:, irhoE, :] = mom*h
+		F[:, irhoY, :] = mom*rhoY/rho
+
+		rho -= eps
+
+		return F
+
+	def AdditionalScalars(self, ScalarName, Up, flag_non_physical):
+		''' Extract state variables '''
+		srho = self.get_state_slice("Density")
+		srhoE = self.get_state_slice("Energy")
+		srhoY = self.get_state_slice("Mixture")
+		smom = self.GetMomentumSlice()
+		rho = Up[:, srho]
+		rhoE = Up[:, srhoE]
+		mom = Up[:, smom]
+		rhoY = Up[:, srhoY]
+
+		''' Common scalars '''
+		gamma = self.gamma
+		R = self.R
+		qo = self.qo
+
+		if flag_non_physical:
+			if np.any(rho < 0.):
+				raise errors.NotPhysicalError
+
+		def get_pressure():
+			# scalar = (gamma - 1.)*(rhoE - 0.5*np.sum(mom*mom, axis=1, keepdims=True)/rho) # just use for storage
+			scalar = (gamma - 1.)*(rhoE - 0.5*np.sum(mom*mom, axis=1, keepdims=True)/rho - qo*rhoY) # just use for storage
+
+			if flag_non_physical:
+				if np.any(scalar < 0.):
+					raise errors.NotPhysicalError
+			return scalar
+		def get_temperature():
+			return get_pressure()/(rho*R)
+
+
+		''' Get final scalars '''
+		sname = self.AdditionalVariables[ScalarName].name
+		if sname is self.AdditionalVariables["Pressure"].name:
+			scalar = get_pressure()
+		elif sname is self.AdditionalVariables["Temperature"].name:
+			# scalar = (gamma - 1.)*(rhoE - 0.5*np.sum(mom*mom, axis=1, keepdims=True)/rho)/(rho*R)
+			scalar = get_temperature()
+		elif sname is self.AdditionalVariables["Entropy"].name:
+			scalar = np.log(get_pressure()/rho**gamma)
+		elif sname is self.AdditionalVariables["InternalEnergy"].name:
+			scalar = rhoE - 0.5*np.sum(mom*mom, axis=1, keepdims=True)/rho
+		elif sname is self.AdditionalVariables["TotalEnthalpy"].name:
+			scalar = (rhoE + get_pressure())/rho
+		elif sname is self.AdditionalVariables["SoundSpeed"].name:
+			# Pressure
+			# P = (gamma - 1.)*(rhoE - 0.5*np.sum(mom*mom, axis=1, keepdims=True)/rho)
+			scalar = np.sqrt(gamma*get_pressure()/rho)
+		elif sname is self.AdditionalVariables["MaxWaveSpeed"].name:
+			# Pressure
+			# P = GetPressure()
+			scalar = np.linalg.norm(mom, axis=1, keepdims=True)/rho + np.sqrt(gamma*get_pressure()/rho)
+		elif sname is self.AdditionalVariables["Velocity"].name:
+			scalar = np.linalg.norm(mom, axis=1, keepdims=True)/rho
+		elif sname is self.AdditionalVariables["MassFraction"].name:
+			scalar = rhoY/rho
+		elif sname is self.AdditionalVariables["SourceTerm"].name:
+			nq = Up.shape[0]
+			x = np.zeros([nq,1])
+			Sp = np.zeros_like(Up) # SourceState is an additive function so source needs to be initialized to zero for each time step
+			Sp = self.SourceState(nq, x, 0., Up, Sp) # [nq,ns]
+			scalar = Sp[:,3].reshape(7,1)
+		elif sname is self.AdditionalVariables["Jacobian"].name:
+			nq = Up.shape[0]
+			x = np.zeros([nq,1])
+			jac = np.zeros([nq,4,4]) # SourceState is an additive function so source needs to be initialized to zero for each time step
+			jac = self.SourceJacobianState(nq, x, 0., Up, jac) # [nq,ns]
+			scalar = jac[:,3,3].reshape(7,1)
+		else:
+			raise NotImplementedError
+
+		return scalar
+
+class Chemistry1D(Chemistry):
+
+	NUM_STATE_VARS = 4
+	dim = 1
+
+	def __init__(self, order, basis, mesh):
+		'''
+		Method: __init__
+		--------------------------------------------------------------------------
+		This method initializes the temperature table. The table uses a
+		piecewise linear function for the constant pressure specific heat 
+		coefficients. The coefficients are selected to retain the exact 
+		enthalpies at the table points.
+		'''
+		super().__init__(order, basis, mesh)
+
+	def set_maps(self):
+		super().set_maps()
+
+		d = {
+			# euler_fcn_type.SmoothIsentropicFlow : euler_fcns.SmoothIsentropicFlow,
+			# euler_fcn_type.MovingShock : euler_fcns.MovingShock,
+			chemistry_fcn_type.DensityWave : chemistry_fcns.DensityWave,
+			chemistry_fcn_type.SimpleDetonation1 : chemistry_fcns.SimpleDetonation1,
+			chemistry_fcn_type.SimpleDetonation3 : chemistry_fcns.SimpleDetonation3,
+
+		}
+
+		self.IC_fcn_map.update(d)
+		self.exact_fcn_map.update(d)
+		self.BC_fcn_map.update(d)
+
+		self.source_map.update({
+			chemistry_source_type.Arrhenius : chemistry_fcns.Arrhenius,
+			chemistry_source_type.Heaviside : chemistry_fcns.Heaviside,
+
+		})
+
+		self.conv_num_flux_map.update({
+			euler_conv_num_flux_type.Roe : euler_fcns.Roe1D,
+		})
+
+	class StateVariables(Enum):
+		__Order__ = 'Density XMomentum Energy Mixture' # only needed in 2.x
+		# LaTeX format
+		Density = "\\rho"
+		XMomentum = "\\rho u"
+		Energy = "\\rho E"
+		Mixture = "\\rho Y"
+
+	def GetStateIndices(self):
+		irho = self.GetStateIndex("Density")
+		irhou = self.GetStateIndex("XMomentum")
+		irhoE = self.GetStateIndex("Energy")
+		irhoY = self.GetStateIndex("Mixture")
+
+		return irho, irhou, irhoE, irhoY
+
+	def get_state_slices(self):
+		srho = self.get_state_slice("Density")
+		srhou = self.get_state_slice("XMomentum")
+		srhoE = self.get_state_slice("Energy")
+		srhoY = self.get_state_slice("Mixture")
+		return srho, srhou, srhoE, srhoY
+
+	def GetMomentumSlice(self):
+		irhou = self.GetStateIndex("XMomentum")
+		smom = slice(irhou, irhou+1)
+
+		return smom
---
+++ src/physics/chemistry/functions.py
@@ -0,0 +1,251 @@
+import code
+from enum import Enum, auto
+import numpy as np
+from scipy.optimize import fsolve, root
+
+from physics.base.data import FcnBase, BCWeakRiemann, BCWeakPrescribed, SourceBase, ConvNumFluxBase
+
+class FcnType(Enum):
+    DensityWave = auto()
+    SimpleDetonation1 = auto()
+    SimpleDetonation3 = auto()
+
+# class BCType(Enum):
+	# SlipWall = auto()
+	# PressureOutlet = auto()
+
+
+class SourceType(Enum):
+    Arrhenius = auto()
+    Heaviside = auto()
+
+class ConvNumFluxType(Enum):
+	Roe = auto()
+
+
+'''
+State functions
+'''
+class DensityWave(FcnBase):
+	def __init__(self, p = 1.0):
+		self.p = p
+
+	def get_state(self, physics, x, t):
+		p = self.p
+		srho, srhou, srhoE, srhoz = physics.get_state_slices()
+		gam = physics.gamma
+		qo = physics.qo
+		Up = np.zeros([x.shape[0], physics.NUM_STATE_VARS])
+		
+		rho = 1.0+0.1*np.sin(2.*np.pi*x)
+		rhou = rho*1.0
+		rhoz = rho*1.0
+		rhoE = (p/(gam-1.))+0.5*rhou**2/rho + qo*rhoz
+		# rhoE = (p/(gam-1.))+0.5*rhou**2/rho
+
+
+		Up[:,srho] = rho
+		Up[:,srhou] = rhou
+		Up[:,srhoE] = rhoE
+		Up[:,srhoz] = rhoz
+
+		return Up
+
+class SimpleDetonation1(FcnBase):
+	def __init__(self, rho_u=1., u_u=0., p_u=1., Y_u=1., xshock = 10.):
+		# These values represent the unburned state.
+		self.rho_u = rho_u
+		self.u_u = u_u
+		self.p_u = p_u
+		self.Y_u = Y_u
+		self.xshock = xshock
+
+	def get_state(self, physics, x, t):
+		# Unpack the unburned state.
+		rho_u = self.rho_u
+		u_u = self.u_u
+		p_u = self.p_u
+		Y_u = self.Y_u
+		xshock = self.xshock
+
+		# Unpack relevant constants from physics class.
+		srho, srhou, srhoE, srhoz = physics.get_state_slices()
+		gam = physics.gamma
+		qo = physics.qo
+		Up = np.zeros([x.shape[0], physics.NUM_STATE_VARS])
+
+		b = -p_u - rho_u*qo * (gam-1.)
+		c = p_u**2 + (2.*(gam-1.) * rho_u*p_u*qo) / (gam+1.)
+
+		p_b = -b + (b**2 - c)**0.5 # burned gas pressure
+		rho_b = rho_u * (p_b * (gam+1.) - p_u) / (gam*p_b) # burned gas density
+		scj = (rho_u*u_u + (gam*p_b*rho_b)**0.5) / rho_u # detonation speed
+		u_b = scj - (gam*p_b / rho_b)**0.5 # burned gas velocity
+		Y_b = 0. # burned gas mixture fraction
+
+		xshock += scj*t
+
+		''' Fill state '''
+		ileft = (x <= xshock).reshape(-1)
+		iright = (x > xshock).reshape(-1)
+
+		# Density
+		Up[iright, srho] = rho_u
+		Up[ileft, srho] = rho_b
+		# Momentum
+		Up[iright, srhou] = rho_u*u_u
+		Up[ileft, srhou] = rho_b*u_b
+		# Energy
+		Up[iright, srhoE] = p_u/(gam-1.) + 0.5*rho_u*u_u*u_u + qo*rho_u*Y_u
+		Up[ileft, srhoE] = p_b/(gam-1.) + 0.5*rho_b*u_b*u_b + qo*rho_b*Y_b
+		# MixtureFraction
+		Up[iright, srhoz] = rho_u*Y_u
+		Up[ileft, srhoz] = rho_b*Y_b
+
+		return Up
+
+class SimpleDetonation3(FcnBase):
+	def __init__(self, uL=np.array([2.,4.,40.,0.]), uR=np.array([0.,0.,0.,0.]), xshock=0.):
+		# These values represent the unburned state.
+		self.uL = uL
+		self.uR = uR
+		self.xshock = xshock
+
+	def get_state(self, physics, x, t):
+
+		uL = self.uL
+		uR = self.uR
+		xshock = self.xshock
+
+		rhoL = uL[0]
+		vL = uL[1]
+		pL = uL[2]
+		yL = uL[3]
+		
+		rhoR = uR[0]
+		vR = uR[1]
+		pR = uR[2]
+		yR = uR[3]
+
+		# Unpack relevant constants from physics class.
+		srho, srhou, srhoE, srhoz = physics.get_state_slices()
+		gam = physics.gamma
+		qo = physics.qo
+		Up = np.zeros([x.shape[0], physics.NUM_STATE_VARS])
+		
+		ileft = (x <= xshock).reshape(-1)
+		iright = (x > xshock).reshape(-1)
+		# Density
+		rhoR = 1. + 0.5*np.sin(2.*x[iright])
+		Up[iright, srho] = rhoR
+		Up[ileft, srho] = rhoL
+		# Momentum
+		Up[iright, srhou] = rhoR*vR
+		Up[ileft, srhou] = rhoL*vL
+		# Energy
+		Up[iright, srhoE] = pR/(gam-1.) + 0.5*rhoR*vR*vR + qo*rhoR*yR
+		Up[ileft, srhoE] = pL/(gam-1.) + 0.5*rhoL*vL*vL + qo*rhoL*yL
+		# MixtureFraction
+		Up[iright, srhoz] = rhoR*yR
+		Up[ileft, srhoz] = rhoL*yL
+
+		return Up
+
+'''
+Boundary conditions
+'''
+
+
+'''
+Source term functions
+'''
+class Arrhenius(SourceBase):
+	def __init__(self, A=16418., b=0.1, Tign=15.):
+		self.A = A
+		self.b = b
+		self.Tign = Tign
+
+	def get_source(self, physics, FcnData, x, t):
+		
+		# Unpack source term constants
+		A = self.A
+		b = self.b
+		Tign = self.Tign
+
+
+		irho, irhou, irhoE, irhoz = physics.GetStateIndices()
+
+		U = FcnData.U
+		T = physics.ComputeScalars("Temperature", U)
+		# K = np.zeros_like(T)
+		# for i in range(len(T)):
+		# 	if T[i]<0.:
+		# 		K[i] = 0.
+		# 	else:
+		# 		K[i] = A * T[i]**b * np.exp(-Tign / T[i])
+		K = A * T**b * np.exp(-Tign / T)
+
+		S = np.zeros_like(U)
+
+		S[:,irhoz] = -K[:].reshape(-1) * U[:,irhoz]
+		
+		return S
+
+	def get_jacobian(self, physics, FcnData, x, t):
+		pass
+
+class Heaviside(SourceBase):
+	def __init__(self, Da=1000., Tign=15.):
+		self.Da = Da
+		self.Tign = Tign
+
+	def get_source(self, physics, FcnData, x, t):
+		
+		# Unpack source term constants
+		Da = self.Da
+		Tign = self.Tign
+
+		irho, irhou, irhoE, irhoz = physics.GetStateIndices()
+
+		U = FcnData.U
+		T = physics.ComputeScalars("Temperature", U)
+		K = np.zeros([U.shape[0]])
+
+		for i in range(len(T)):
+			if T[i] >= Tign:
+				K[i] = Da
+			else:
+				K[i] = 0.
+
+		S = np.zeros_like(U)
+		S[:,irhoz] = -K[:] * U[:,irhoz]
+		
+		return S
+
+	def get_jacobian(self, physics, FcnData, x, t):
+
+		# Unpack source term constants
+		Da = self.Da
+		Tign = self.Tign
+
+		U = FcnData.U
+		
+		irho, irhou, irhoE, irhoY = physics.GetStateIndices()
+
+		T = physics.ComputeScalars("Temperature", U)
+		K = np.zeros([U.shape[0]])
+
+		for i in range(len(T)):
+			if T[i] >= Tign:
+				K[i] = Da
+			else:
+				K[i] = 0.
+
+		jac = np.zeros([U.shape[0], U.shape[-1], U.shape[-1]])
+		jac[:, irhoY, irhoY] = -K
+
+		return jac
+
+'''
+Numerical flux functions
+'''
