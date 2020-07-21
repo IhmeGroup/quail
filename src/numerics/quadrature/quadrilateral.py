a04abea75223b3dc4346d90b223c4836a95a88b7
Brett Bornhoft <bbornhoft@gmail.com>
2020-07-20 21:12:56 -0700

    Quadrature comments

--- src/numerics/quadrature/quadrilateral.py
+++ src/numerics/quadrature/quadrilateral.py
@@ -6,7 +6,6 @@ import numerics.quadrature.segment as qseg
 def get_quadrature_points_weights(order, quad_type, forced_pts=None):
 
 	# Get quadrature points and weights for segment
-	# Only Gauss-Legendre for now
 	try:
 		fpts = int(np.sqrt(forced_pts))
 	except:
--- src/numerics/quadrature/segment.py
+++ src/numerics/quadrature/segment.py
@@ -1,13 +1,41 @@
+# ------------------------------------------------------------------------ #
+#
+#       File : numerics/quadrature/segment.py
+#
+#       Contains functions to evaluate quadrature for segment shapes
+#
+#       Authors: Eric Ching and Brett Bornhoft
+#
+#       Created: January 2020
+#      
+# ------------------------------------------------------------------------ #
 import numpy as np
-import general
 import math
 import code
-from scipy.linalg import eigh_tridiagonal
+from scipy.linalg import eigh_tridiagonal, solve_banded, solve
+
 import itertools
 
-def get_quadrature_points_weights(order, quad_type, forced_pts=None):
+import general
 
-	# Only Gauss-Legendre for now
+
+def get_quadrature_points_weights(order, quad_type, forced_pts=None):
+    '''
+    Method: get_quadrature_points_weights
+    --------------------------------------
+    Depending on the QuadratureType enum calculate the appropriate 
+    function to obtain quadrature points and weights
+
+    INPUTS: 
+        order: solution order [int]
+        quad_type: Enum that points to the appropriate quadrature calc [enum]
+        forced_pts: [OPTIONAL] number of points if forcing nodes to be 
+                    equal to quad_pts is turned on [int]
+
+    OUTPUTS:
+        qpts: quadrature point coordinates [nq, dim]
+        qwts: quadrature weights [nq, 1]
+    '''
 	if quad_type == general.QuadratureType.GaussLegendre:
 		qpts, qwts = get_quadrature_gauss_legendre(order)
 	elif quad_type == general.QuadratureType.GaussLobatto:
@@ -17,39 +45,81 @@ def get_quadrature_points_weights(order, quad_type, forced_pts=None):
 
 	return qpts, qwts
 
+
 def get_quadrature_gauss_legendre(order):
-	# if order is even, increment by 1
-	if order % 2 == 0:
+    '''
+    Method: get_quadrature_gauss_legendre
+    --------------------------------------
+    Calculate the quadrature points and weights using Gauss Legendre rules
+
+    INPUTS: 
+        order: solution order [int]
+
+    OUTPUTS:
+        qpts: quadrature point coordinates [nq, dim]
+        qwts: quadrature weights [nq, 1]
+    '''
+	if order % 2 == 0: # if order is even, add 1
 		order += 1
 
 	npts = (order + 1)//2
 
+    # use built-in numpy Gauss Legendre functions
 	qpts, qwts = np.polynomial.legendre.leggauss(npts)
 
 	qpts.shape = -1,1
 	qwts.shape = -1,1
 
-	return qpts, qwts
+	return qpts, qwts # [nq, 1] and [nq, 1]
+
 
 def get_quadrature_gauss_lobatto(order, forced_pts=None):
+    '''
+    Method: get_quadrature_gauss_lobatto
+    --------------------------------------
+    Calculate the quadrature points and weights using Gauss Lobatto rules
 
-    if order == 1:
-        qpts, qwts = get_quadrature_points_weights(order, general.QuadratureType["GaussLegendre"])
+    INPUTS: 
+        order: solution order [int]
+        forced_pts: [OPTIONAL] number of points if forcing nodes to be 
+                    equal to quad_pts is turned on [int]
+
+    OUTPUTS:
+        qpts: quadrature point coordinates [nq, dim]
+        qwts: quadrature weights [nq, dim]
+    '''
+    if order == 1: # Gauss Lobatto not defined for order = 1
+        qpts, qwts = get_quadrature_points_weights(order, 
+            general.QuadratureType["GaussLegendre"])
     else:
         if forced_pts != None:
-            # Get order from forced_pts -> pass order.
+            # get order from forced_pts -> pass order.
             order = 2*forced_pts - 3
             qpts, qwts = gauss_lobatto(order)
         else:
+            # use order argument in function call
             qpts, qwts = gauss_lobatto(order)
 
         qpts=qpts.reshape(qpts.shape[0],1)
         qwts=qwts.reshape(qwts.shape[0],1)
 
-    return qpts, qwts
+    return qpts, qwts # [nq, 1] and [nq, 1]
+
 
 def gauss_lobatto(order):
-    if order % 2 == 0:
+    '''
+    Method: gauss_lobatto
+    -----------------------
+    Evaluate quadrature with Gauss Lobatto rules
+
+    INPUTS: 
+        order: solution order [int]
+
+    OUTPUTS:
+        qpts: quadrature point coordinates [nq,]
+        qwts: quadrature weights [nq,]
+    '''
+    if order % 2 == 0: # if order is even, add 1
         order += 1
     npts = int((order + 3)/2)
 
@@ -60,14 +130,25 @@ def gauss_lobatto(order):
 
 
 def get_lobatto_pts_wts(alpha, beta, xl1, xl2):
-    """Compute the Lobatto nodes and weights with the preassigned node xl1, xl2.
+    '''
+    Method: get_lobatto_pts_wts
+    ------------------------------
+    Compute the Lobatto nodes and weights with the preassigned node xl1, xl2.
     Based on the section 7 of the paper
         Some modified matrix eigenvalue problems,
         Gene Golub,
-        SIAM Review Vol 15, No. 2, April 1973, pp.318--334,
-    """
-    from scipy.linalg import solve_banded, solve
+        SIAM Review Vol 15, No. 2, April 1973, pp.318--334
 
+    INPUTS:
+        alpha: constant used to find recurrence coefficients
+        beta: constant used to find recurrence coefficients
+        xl1: end node [-1]
+        xl2: end node [+1]
+
+    OUTPUTS:
+        x: quadrature points
+        w: quadrature weights
+    '''
     n = len(alpha) - 1
     en = np.zeros(n)
     en[-1] = 1
@@ -86,24 +167,32 @@ def get_lobatto_pts_wts(alpha, beta, xl1, xl2):
     betal = beta
     betal[-1] = ab[1]
     x, w = scheme_from_rc(alphal, betal)
+
     return x, w
 
+
 def scheme_from_rc(alpha, beta):
+    '''
+    Helper function for calculating Gauss Lobatto quadrature
+    '''
     alpha = alpha.astype(np.float64)
     beta = beta.astype(np.float64)
     x, V = eigh_tridiagonal(alpha, np.sqrt(beta[1:]))
     w = beta[0] * V[0, :] ** 2
     return x, w
 
+
 def jacobi(n, alpha, beta):
-    """Generate the recurrence coefficients a_k, b_k, c_k in
+    '''
+    Method: jacobi
+    ------------------
+    Generate the recurrence coefficients a_k, b_k, c_k in
 
     P_{k+1}(x) = (a_k x - b_k)*P_{k}(x) - c_k P_{k-1}(x)
 
-    for the Jacobi polynomials which are orthogonal on [-1, 1] with respect to the
-    weight w(x)=[(1-x)^alpha]*[(1+x)^beta]; see
-    <https://en.wikipedia.org/wiki/Jacobi_polynomials#Recurrence_relations>.
-    """
+    for the Jacobi polynomials which are orthogonal on [-1, 1] with respect 
+    to the weight w(x)=[(1-x)^alpha]*[(1+x)^beta]; 
+    '''
     iterator = Jacobi(alpha, beta)
     p0 = iterator.p0
     lst = list(itertools.islice(iterator, n))
@@ -111,6 +200,7 @@ def jacobi(n, alpha, beta):
     b = np.array([item[1] for item in lst])
     c = np.array([item[2] for item in lst])
     return b, c
+
 
 class Jacobi:
     def __init__(self, alpha, beta):
@@ -124,6 +214,7 @@ class Jacobi:
 
     def __next__(self):
         return self.iterator.__next__()
+
 
 class Monic:
     def __init__(self, alpha, beta):
@@ -162,14 +253,6 @@ class Monic:
                 (2 * N + alpha + beta) * (2 * N + alpha + beta + 2),
             )
 
-        # c[0] is not used in the actual recurrence, but is often defined as the
-        # integral of the weight function of the domain, i.e.,
-        # ```
-        # int_{-1}^{+1} (1-x)^a * (1+x)^b dx =
-        #     2^(a+b+1) * Gamma(a+1) * Gamma(b+1) / Gamma(a+b+2).
-        # ```
-        # Note also that we have the treat the case N==1 separately to avoid division by
-        # 0 for alpha=beta=-1/2.
         if N == 0:
             c = self.int_1
         elif N == 1:
