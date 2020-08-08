07617dc2a4cba56beb629aef8984cddc9f05be0b
alilasemi <alasem2@illinois.edu>
2020-08-08 13:46:48 -0500

    Began work on refinement flags

--- src/meshing/adapt.py
+++ src/meshing/adapt.py
@@ -12,31 +12,40 @@ def adapt(solver, physics, mesh, stepper):
     stepper - Stepper object (timestepping/stepper.py)
     """
 
-    # Append to the end of U
-    physics.U = np.append(physics.U, [physics.U[-1,:,:]], axis=0)
-    # Delete residual
-    stepper.R = None
-    print("appended U:")
-    print(physics.U)
-    # Add new mesh node
-    mesh.node_coords = np.append(mesh.node_coords, [[-.5,0]], axis=0)
-    # Add new element
-    mesh.elements.append(mesh_defs.Element(4))
-    # Set element's node ids, coords, and neighbors
-    mesh.elements[4].node_ids = np.array([0,1,6])
-    mesh.elements[4].node_coords = np.array([[-1., -1.], [ 0., -1.], [-.5, 0.]])
-    mesh.elements[4].face_to_neighbors = np.array([-1,-1,-1])
-    # Make the first element smaller to make space
-    mesh.elements[0].node_ids = np.array([0,6,3])
-    mesh.elements[0].node_coords = np.array([[-1., -1.], [ -.5, 0.], [-1., 1.]])
-    mesh.elements[0].face_to_neighbors = np.array([-1,-1,-1])
-    # Increment number of elements
-    mesh.num_elems += 1
-    print("num_elems: ", mesh.num_elems)
-    solver.elem_operators.x_elems = np.append(solver.elem_operators.x_elems, [solver.elem_operators.x_elems[-1,:,:]], axis=0)
-    # Call compute operators
-    solver.precompute_matrix_operators()
-    print(solver.elem_operators.x_elems)
+    # Array of flags for which elements to be split
+    needs_refinement = np.zeros(mesh.num_elems, dtype=bool)
+    # Just split element 0
+    needs_refinement[0] = True
+
+    # Loop over all elements
+    for elem in range(mesh.num_elems):
+        # Only refine elements that need refinement
+        if needs_refinement[elem]:
+
+            # Append to the end of U
+            physics.U = np.append(physics.U, [physics.U[-1,:,:]], axis=0)
+            # Delete residual
+            stepper.R = None
+
+            # Add new mesh node
+            mesh.node_coords = np.append(mesh.node_coords, [[-.5,0]], axis=0)
+            # Add new element
+            mesh.elements.append(mesh_defs.Element(4))
+            # Set element's node ids, coords, and neighbors
+            mesh.elements[4].node_ids = np.array([0,1,6])
+            mesh.elements[4].node_coords = np.array([[-1., -1.], [ 0., -1.], [-.5, 0.]])
+            mesh.elements[4].face_to_neighbors = np.array([-1,-1,-1])
+            # Make the first element smaller to make space
+            mesh.elements[0].node_ids = np.array([0,6,3])
+            mesh.elements[0].node_coords = np.array([[-1., -1.], [ -.5, 0.], [-1., 1.]])
+            mesh.elements[0].face_to_neighbors = np.array([-1,-1,-1])
+            # Increment number of elements
+            mesh.num_elems += 1
+            print("num_elems: ", mesh.num_elems)
+            solver.elem_operators.x_elems = np.append(solver.elem_operators.x_elems, [solver.elem_operators.x_elems[-1,:,:]], axis=0)
+            # Call compute operators
+            solver.precompute_matrix_operators()
+            print(solver.elem_operators.x_elems)
 
     # Just printing random things to check them out
     print(mesh.node_coords)
