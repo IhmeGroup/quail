71b0adc8f2c59bf9194b6b09fe0d57b2772c7336
Brett Bornhoft <bbornhoft@gmail.com>
2020-11-09 20:35:19 -0800

    inviscid burgers test case

--- examples/scalar/1D/inviscid_burgers/post_process.py
+++ examples/scalar/1D/inviscid_burgers/post_process.py
@@ -8,9 +8,6 @@ import processing.readwritedatafiles as readwritedatafiles
 plot.prepare_plot()
 
 
-
-#plot.plot_solution(mesh, physics, solver, "Scalar", plot_exact=True, plot_numerical=False, create_new_figure=True, fmt='k-')
-
 # fname = "Ref_final.pkl"
 # solver = readwritedatafiles.read_data_file(fname)
 # print('Solution Final Time:', solver.time)
@@ -26,7 +23,7 @@ plot.prepare_plot()
 # plot.plot_solution(mesh, physics, solver, "Scalar", plot_IC=True, 
 #			plot_numerical=False, create_new_figure=False, fmt='k--')
 
-fname = "NO_LIM.pkl"
+fname = "Data_final.pkl"
 solver = readwritedatafiles.read_data_file(fname)
 print('Solution Final Time:', solver.time)
 
@@ -39,20 +36,22 @@ plot.plot_solution(mesh, physics, solver, "Scalar", plot_numerical=True,
 			equidistant_pts=True, include_mesh=False, regular_2D=False, 
 			equal_AR=False)
 
+plot.plot_solution(mesh, physics, solver, "Scalar", plot_exact=True,
+		plot_numerical=False, create_new_figure=False, fmt='k-')
 
 fname = "Data_final.pkl"
 solver = readwritedatafiles.read_data_file(fname)
 print('Solution Final Time:', solver.time)
 
 # Unpack
-mesh = solver.mesh
-physics = solver.physics
+# mesh = solver.mesh
+# physics = solver.physics
 
-plot.plot_solution(mesh, physics, solver, "Scalar", plot_numerical=True, 
-			plot_exact=False, plot_IC=False, create_new_figure=False, 
-			ylabel=None, fmt='bo', legend_label="WENO Limiter", equidistant_pts=True, 
-			include_mesh=False, regular_2D=False, equal_AR=False)
+# plot.plot_solution(mesh, physics, solver, "Scalar", plot_numerical=True, 
+# 			plot_exact=False, plot_IC=False, create_new_figure=False, 
+# 			ylabel=None, fmt='bo', legend_label="WENO Limiter", equidistant_pts=True, 
+# 			include_mesh=False, regular_2D=False, equal_AR=False)
 
-plot.save_figure(file_name='inv_burgers_weno_testing', file_type='pdf', crop_level=2)
+# plot.save_figure(file_name='inv_burgers_weno_testing', file_type='pdf', crop_level=2)
 
 plot.show_plot()
