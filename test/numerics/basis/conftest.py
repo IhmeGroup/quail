af1505e6033383609bf5a8f18e6ac327e07c673d
alilasemi <alasemi@stanford.edu>
2020-12-22 16:50:32 -0600

    Generalize nodal basis test to all element shapes

--- test/numerics/basis/conftest.py
+++ test/numerics/basis/conftest.py
@@ -4,8 +4,6 @@ sys.path.append('src')
 import numpy as np
 import pytest
 
-import numerics.basis.basis as basis_defs
-
 @pytest.fixture
-def lagrange_basis(order):
-    yield basis_defs.LagrangeTri(order)
+def basis(Basis, order):
+    yield Basis(order)
