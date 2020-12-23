7fa969e66ec2705e0f1509c41101ce680bbcfd9b
alilasemi <alasemi@stanford.edu>
2020-12-23 12:09:22 -0600

    Generate regression test data for multiple tests

--- test/end_to_end/generate_test_data.py
+++ test/end_to_end/generate_test_data.py
@@ -15,38 +15,51 @@ import solver.ADERDG as ADERDG
 
 def generate_regression_test_data():
 
-    datafile_name = 'regression_data.npy'
-
     # Get script directory
     script_dir = os.getcwd()
-    # Move to the test case directory
-    os.chdir('scalar/1D/constant_advection')
-    # Call the Quail executable
-    result = subprocess.check_output([
-            f'{script_dir}/../../src/quail', 'constant_advection.py',
-            ], stderr=subprocess.STDOUT)
-    # Print results of run
-    print(result.decode('utf-8'))
 
-    # Open resulting data file
-    with open('Data_final.pkl', 'rb') as f:
-        # Load data
-        solver = pickle.load(f)
-        # Save final solution to file
-        append_to_file(datafile_name, solver.state_coeffs)
+    # Names of case directories
+    case_dirs = [
+            'scalar/1D/constant_advection',
+            'scalar/1D/inviscid_burgers',
+            ]
+    # Add full path
+    case_dirs = [f'{script_dir}/{case_dir}' for case_dir in case_dirs]
+    n_cases = len(case_dirs)
+
+    # Name and path of data file which stores the regression test data
+    datafile_name = f'{script_dir}/regression_data.npy'
+    # If this already exists, delete it
+    os.system(f'rm -f {datafile_name}')
+
+    # Loop over all case directories
+    results = []
+    for case_dir in case_dirs:
+        # Move to the test case directory
+        os.chdir(case_dir)
+        # Call the Quail executable
+        result = subprocess.check_output([
+                f'{script_dir}/../../src/quail', 'input_file.py',
+                ], stderr=subprocess.STDOUT)
+        # Print results of run
+        print(result.decode('utf-8'))
+
+        # Open resulting solution file
+        with open('Data_final.pkl', 'rb') as solutionfile:
+            # Load data
+            solver = pickle.load(solutionfile)
+            # Save final solution
+            results.append(solver.state_coeffs)
+
+    # Save results to the regression test datafile
+    with open(datafile_name, 'wb') as datafile:
+        for data in results: np.save(datafile, data)
 
     # Read file
+    output = []
     with open(datafile_name, 'rb') as datafile:
-        a = np.load(datafile)
-        #b = np.load(datafile)
-        breakpoint()
-
-
-def append_to_file(datafile_name, data):
-    with open(datafile_name, 'wb') as datafile:
-        np.save(datafile, data)
-
-
+        for i in range(n_cases):
+            results.append(np.load(datafile))
 
 
 if __name__ == "__main__":
