903f2b7a271fc3e640aa44cb5821e79d03cf371f
alilasemi <alasemi@stanford.edu>
2021-01-06 16:12:22 -0800

    Add remaining test cases and regression data

---
+++ test/end_to_end/cases/euler/2D/isentropic_vortex/input_file.py
@@ -0,0 +1,49 @@
+TimeStepping = {
+	"FinalTime" : 1.0,
+	"TimeStepSize" : 0.05,
+	"TimeStepper" : "LSRK4",
+}
+
+Numerics = {
+	"SolutionOrder" : 2,
+	"SolutionBasis" : "LagrangeTri",
+	"ElementQuadrature" : "Dunavant",
+	"FaceQuadrature" : "GaussLegendre",
+}
+
+Mesh = {
+	"ElementShape" : "Triangle",
+	"NumElemsX" : 5,
+	"NumElemsY" : 5,
+	"xmin" : -5.,
+	"xmax" : 5.,
+	"ymin" : -5.,
+	"ymax" : 5.,
+	"PeriodicBoundariesX" : ["x2", "x1"],
+}
+
+Physics = {
+	"Type" : "Euler",
+	"ConvFluxNumerical" : "LaxFriedrichs",
+	"GasConstant" : 1.,
+}
+
+InitialCondition = {
+	"Function" : "IsentropicVortex",
+}
+
+ExactSolution = InitialCondition.copy()
+
+d = {
+		"BCType" : "StateAll",
+		"Function" : "IsentropicVortex",
+}
+
+BoundaryConditions = {
+	"y1" : d,
+	"y2" : d,
+}
+
+Output = {
+	"AutoPostProcess" : False,
+}
--- test/end_to_end/list_of_cases.py
+++ test/end_to_end/list_of_cases.py
@@ -1,10 +1,14 @@
 # A list containing the directories of each test case. When adding a new test
 # case, the directory needs to be added to this list.
 case_dirs = [
-        'scalar/1D/constant_advection',
-        'scalar/1D/inviscid_burgers',
-        'scalar/1D/damping_sine_wave/dg',
-        'scalar/1D/damping_sine_wave/ader',
-        'scalar/1D/damping_sine_wave/splitting',
-        'scalar/2D/constant_advection',
+        'cases/scalar/1D/constant_advection',
+        'cases/scalar/1D/inviscid_burgers',
+        'cases/scalar/1D/damping_sine_wave/dg',
+        'cases/scalar/1D/damping_sine_wave/ader',
+        'cases/scalar/1D/damping_sine_wave/splitting',
+        'cases/scalar/2D/constant_advection',
+        'cases/euler/1D/moving_shock',
+        'cases/euler/1D/smooth_isentropic_flow',
+        'cases/euler/2D/flow_over_bump',
+        'cases/euler/2D/isentropic_vortex',
         ]
