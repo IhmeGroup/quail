6a442282ca2b3ff55e0a082b3caafea23b717a67
Brett Bornhoft <bbornhoft@gmail.com>
2021-02-15 14:46:22 -0800

    Updated Setup for the 0D-PSR model problem

--- examples/scalar/1D/model_psr/custom_user_function.py
+++ examples/scalar/1D/model_psr/custom_user_function.py
@@ -1,10 +1,15 @@
+import numerics.helpers.helpers as helpers
+
 def custom_user_function(solver):
 
 	# Unpack
-	Uq = solver.state_coeffs
+	Uc = solver.state_coeffs
+	basis_val = solver.elem_helpers.basis_val
+	Uq = helpers.evaluate_state(Uc, basis_val)
+	
 	time_hist = open('time_hist.txt', 'a')
 	s = str(solver.time)
-	s1 = str(Uq[0,0,0])
+	s1 = str(Uq[0,1,0])
 	time_hist.write(s)
 	time_hist.write(' , ')
 	time_hist.write(s1)
