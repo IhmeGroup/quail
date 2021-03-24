5c1bfb634e6cda95c76733dd7c78d23e535635d4
Brett Bornhoft <bbornhoft@gmail.com>
2021-03-24 14:49:42 -0700

    Model PSR problem updates:
    - clean up post-processing files
    - clean up input deck

---
+++ examples/scalar/1D/model_psr/case_a.py
@@ -0,0 +1,38 @@
+import code
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import rc
+
+font = {'family' : 'serif',
+        'serif'  : 'Times',
+        'size'   : 14}
+
+plt.rc('font', **font)
+rc('text',usetex=True)
+def extract_data(filename):
+    infile = open(filename, 'r')
+    endLine = infile.readlines()
+    #infile.readline() # skip the first line
+    time = []
+    pmax = []
+    for line in endLine:
+        data = line.split()
+        time.append(float(data[0]))
+        pmax.append(float(data[2]))
+    infile.close()
+
+    time = np.array(time)
+    pmax = np.array(pmax)
+    return time, pmax
+
+fig, ax = plt.subplots()
+
+plt.ylabel('$T$')
+plt.xlabel('$t/Da$')
+
+Da = 15.89
+time, T = extract_data('time_hist.txt')
+ax.plot(time/Da, T, label='Solution')
+plt.grid(True)
+ax.legend()
+plt.show()
