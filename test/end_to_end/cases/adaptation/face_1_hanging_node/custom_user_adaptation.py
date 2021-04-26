6ef16521fbb855a4be2006d03dc7f0ec2140a1a2
alilasemi <alasemi@stanford.edu>
2021-04-25 20:39:51 -0700

    Add face 1 hanging node test

--- test/end_to_end/cases/adaptation/face_1_hanging_node/custom_user_adaptation.py
+++ test/end_to_end/cases/adaptation/face_1_hanging_node/custom_user_adaptation.py
@@ -4,22 +4,14 @@ import numerics.helpers.helpers as helpers
 
 def custom_user_adaptation(solver):
     '''
+    Split element 0 along face 0 after the first iteration.
     '''
     # Current iteration number
     i = int(solver.time / solver.stepper.dt) - 1
 
     if i == 0:
         refine_IDs = np.array([1])
-        split_face_IDs = np.array([0])
-    #elif i == 1:
-    #    refine_IDs = np.array([0])
-    #    split_face_IDs = np.array([0])
-    #elif i == 2:
-    #    refine_IDs = np.array([2])
-    #    split_face_IDs = np.array([0])
-    #elif i == 3:
-    #    refine_IDs = np.array([0])
-    #    split_face_IDs = np.array([0])
+        split_face_IDs = np.array([1])
     else:
         refine_IDs = np.array([])
         split_face_IDs = np.array([])
