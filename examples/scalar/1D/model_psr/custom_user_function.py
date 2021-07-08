6906ba49a4879f561f5c061eb27f6ff3d6c111d0
Brett Bornhoft <bbornhoft@gmail.com>
2021-07-08 13:23:48 -0700

    Print jacobian to a file using the custom user function. This allows for later processing.

--- examples/scalar/1D/model_psr/custom_user_function.py
+++ examples/scalar/1D/model_psr/custom_user_function.py
@@ -5,13 +5,17 @@ def custom_user_function(solver):
 	if solver.time>=tstart:
 		# Unpack
 		Uc = solver.state_coeffs
+		jac = solver.physics.jac
 		basis_val = solver.elem_helpers.basis_val
 		Uq = helpers.evaluate_state(Uc, basis_val)
 		time_hist = open('time_hist.txt', 'a')
 		s = str(solver.time)
 		s1 = str(Uq[0,0,0])
+		s2 = str(jac[0,0,0,0])
 		time_hist.write(s)
 		time_hist.write(' , ')
 		time_hist.write(s1)
+		time_hist.write(' , ')
+		time_hist.write(s2)
 		time_hist.write('\n')
 		time_hist.close()
