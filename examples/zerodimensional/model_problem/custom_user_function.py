db0bf9f8b87c01f0430be37930f7a7ea1202b2ce
Brett Bornhoft <bbornhoft@gmail.com>
2021-07-26 13:04:35 -0700

    Add the model problem with README and scripts for generating convergence studies

---
+++ examples/zerodimensional/model_problem/custom_user_function.py
@@ -0,0 +1,20 @@
+import numerics.helpers.helpers as helpers
+import pickle
+import numpy as np
+
+def custom_user_function(solver):
+	tstart = 0.
+	if solver.time>=tstart:
+		# Unpack
+		Uc = solver.state_coeffs
+		basis_val = solver.elem_helpers.basis_val
+		Uq = helpers.evaluate_state(Uc, basis_val)
+	
+		time_hist = open('time_hist.txt', 'a')
+		s = str(solver.time)
+		s1 = str(Uq[0, 0, 0])
+		time_hist.write(s)
+		time_hist.write(' , ')
+		time_hist.write(s1)
+		time_hist.write('\n')
+		time_hist.close()
---
+++ examples/zerodimensional/model_problem/model_problem.py
@@ -0,0 +1,55 @@
+import numpy as np
+
+tfinal = 1.0
+timescheme = 'ODEIntegrator'
+timestep = 0.00048828125
+solver = 'DG'
+order = 0
+
+prefix = 'Data'
+TimeStepping = {
+	"InitialTime" : 0.,
+	
+	"FinalTime" : tfinal,
+	"TimeStepSize" : timestep,
+	"TimeStepper" : timescheme,
+	"ODEScheme" : "RK4",
+}
+
+Numerics = {
+	"SolutionOrder" : order,
+	"SolutionBasis" : "LagrangeSeg",
+	"Solver" : solver,
+}
+
+Mesh = {
+	"File" : None,
+	"ElementShape" : "Segment",
+	"NumElemsX" : 1,
+	"xmin" : -1.,
+	"xmax" : 1.,
+	"PeriodicBoundariesX" : ["x1", "x2"],
+}
+
+Physics = {
+	"Type" : "ModelProblem",
+	"ConvFluxNumerical" : "LaxFriedrichs",
+}
+
+InitialCondition = {
+	"Function" : "Uniform",
+	"state" : np.array([1.0]),
+}
+
+SourceTerms = {
+	"SimpleSource" : {
+		"Function" : "SimpleSource",
+		"nu" : -1.0,
+		"source_treatment" : "Implicit",
+	}
+}
+
+Output = {
+	"AutoPostProcess" : False,
+	"WriteFinalSolution" : True,
+}
