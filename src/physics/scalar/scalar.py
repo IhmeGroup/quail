595bb488ece370b508bf609f0099e3dab0f1e368
Brett Bornhoft <bbornhoft@gmail.com>
2021-09-08 17:01:42 -0700

    Start adding infrastructure for advection-diffusion scalar case

--- src/physics/scalar/scalar.py
+++ src/physics/scalar/scalar.py
@@ -175,6 +175,112 @@ class ConstAdvScalar2D(ConstAdvScalar):
 
 		return F, None
 
+class ConstAdvDiffScalar(base.PhysicsBase):
+	'''
+	This class corresponds to scalar advection/diffusion with a constant 
+	velocity and diffusion coefficient.
+
+	It inherits attributes and methods from the PhysicsBase class. See
+	PhysicsBase for detailed comments of attributes and methods. This
+	class should not be instantiated directly. Instead, the 1D and 2D
+	variants, which inherit from this class (see below), should be
+	instantiated.
+
+	Additional methods and attributes are commented below.
+
+	Attributes:
+	-----------
+	c: float or numpy array
+		advection velocity
+	al: float or numpy array
+		diffusion coefficient
+	cspeed: float
+		advection speed
+	'''
+	NUM_STATE_VARS = 1
+	PHYSICS_TYPE = general.PhysicsType.ConstAdvDiffScalar
+
+	def __init__(self, mesh):
+		super().__init__(mesh)
+		self.c = 0.
+		self.cspeed = 0.
+
+	def set_maps(self):
+		super().set_maps()
+
+		# self.source_map.update({
+		# 	scalar_source_type.SimpleSource : scalar_fcns.SimpleSource,
+		# })
+
+	class StateVariables(Enum):
+		Scalar = "u"
+
+	class AdditionalVariables(Enum):
+	    MaxWaveSpeed = "\\lambda"
+
+	def get_conv_flux_interior(self, Uq):
+		c = self.c
+		F = np.expand_dims(c*Uq, axis=-1)
+
+		return F, None
+
+	def get_diff_flux_interior(self, Uq, gUq)::
+		al = self.al
+		F = al * gUq
+
+		return F
+		
+	def compute_additional_variable(self, var_name, Uq, flag_non_physical):
+		sname = self.AdditionalVariables[var_name].name
+
+		if sname is self.AdditionalVariables["MaxWaveSpeed"].name:
+			# Max wave speed is the advection speed
+			scalar = np.full([Uq.shape[0], 1, 1], self.cspeed)
+		else:
+			raise NotImplementedError
+
+		return scalar
+
+
+class ConstAdvDiffScalar1D(ConstAdvScalar):
+	'''
+	This class corresponds to 1D scalar advection/diffusion with a constant
+	velocity and diffusion coefficient. 
+
+	It inherits attributes and methods from the ConstAdvDiffScalar class. See
+	ConstAdvScalar for detailed comments of attributes and methods.
+
+	Additional methods and attributes are commented below.
+	'''
+	NDIMS = 1
+
+	def set_maps(self):
+		super().set_maps()
+
+		d = {
+			scalar_fcn_type.Gaussian : scalar_fcns.Gaussian,
+		}
+
+		self.IC_fcn_map.update(d)
+		self.exact_fcn_map.update(d)
+		self.BC_fcn_map.update(d)
+
+	def set_physical_params(self, ConstVelocity=1., DiffCoefficient=1.):
+		'''
+		This method sets physical parameters.
+
+		Inputs:
+		-------
+			ConstVelocity: constant advection velocity
+			DiffCoefficient: constant diffusion coefficient
+
+		Outputs:
+		--------
+			self: physical parameters set
+		'''
+		self.c = ConstVelocity
+		self.cspeed = np.abs(self.c)
+		self.al = DiffCoefficient
 
 class Burgers1D(base.PhysicsBase):
 	'''
