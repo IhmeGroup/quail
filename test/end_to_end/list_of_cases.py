636fa35d914bd6b6766351595bfd298eba4be893
Brett Bornhoft <bbornhoft@gmail.com>
2021-11-10 15:17:47 -0800

    add rtol and atol to lists

--- test/end_to_end/list_of_cases.py
+++ test/end_to_end/list_of_cases.py
@@ -1,3 +1,7 @@
+# Default tolerances
+rtol = 1e-14
+atol = 1e-14
+
 # All currently available markers
 one_d = 'one_d'
 two_d = 'two_d'
@@ -11,25 +15,28 @@ diffusion = 'diffusion'
 # test case, the directory needs to be added to this list as well as a list of
 # markers for it.
 case_dirs = {
-	'scalar/1D/constant_advection' : [one_d, dg],
-	'scalar/1D/inviscid_burgers' : [one_d, dg],
-	'scalar/1D/damping_sine_wave/dg' : [one_d, dg, source],
-	'scalar/1D/damping_sine_wave/ader' : [one_d, ader, source],
-	'scalar/1D/damping_sine_wave/splitting' : [one_d, dg, splitting, source],
-	'scalar/2D/constant_advection/dg' : [two_d, dg],
-	'euler/1D/moving_shock' : [two_d, dg],
-	'euler/1D/smooth_isentropic_flow' : [two_d, dg],
-	'euler/2D/flow_over_bump' : [two_d, dg],
-	'euler/2D/isentropic_vortex' : [two_d, dg],
-	'scalar/1D/constant_advection_diffusion/dg' : [one_d, dg, diffusion],
+	'scalar/1D/constant_advection' : [[one_d, dg], [rtol, atol]],
+	'scalar/1D/inviscid_burgers' : [[one_d, dg], [rtol, atol]],
+	'scalar/1D/damping_sine_wave/dg' : [[one_d, dg, source], [rtol, atol]],
+	'scalar/1D/damping_sine_wave/ader' : [[one_d, ader, source], [rtol, atol]],
+	'scalar/1D/damping_sine_wave/splitting' : [[one_d, dg, splitting, source], 
+		[rtol, atol]],
+	'scalar/2D/constant_advection' : [[two_d, dg], [1e-9, 1e-13]],
+	'euler/1D/moving_shock' : [[one_d, dg], [rtol, atol]],
+	'euler/1D/smooth_isentropic_flow' : [[two_d, dg], [rtol, atol]],
+	'euler/2D/flow_over_bump' : [[two_d, dg], [rtol, atol]],
+	'euler/2D/isentropic_vortex' : [[two_d, dg], [rtol, atol]],
+	'scalar/1D/constant_advection_diffusion/dg' : [[one_d, dg, diffusion],
+		[rtol, atol]],
 	'scalar/1D/constant_advection_diffusion/ader/interpolate' : \
-		[one_d, ader, diffusion],
+		[[one_d, ader, diffusion], [rtol, atol]],
 	'scalar/1D/constant_advection_diffusion/ader/l2projection' : \
-		[one_d, ader, diffusion],	
+		[[one_d, ader, diffusion], [rtol, atol]],
 	'scalar/2D/constant_advection_diffusion/dg' : \
-		[two_d, dg, diffusion],	
-	'scalar/2D/constant_advection/ader' : [two_d, ader],
-	'navierstokes/2D/manufactured_solution/dg' : [two_d, ader, diffusion],
+		[[two_d, dg, diffusion], [rtol, atol]],
+	'scalar/2D/constant_advection/ader' : [[two_d, ader], [rtol, atol]],
+	'navierstokes/2D/manufactured_solution/dg' : [[two_d, ader, diffusion],
+		[rtol, atol]],
 	'scalar/2D/constant_advection_diffusion/ader' : \
-		[two_d, ader, diffusion],
+		[[two_d, ader, diffusion], [rtol, atol]],
 	}
