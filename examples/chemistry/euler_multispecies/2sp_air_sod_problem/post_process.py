502678d45160af012c46e7812d4490125a3b6ebe
Brett Bornhoft <bbornhoft@gmail.com>
2021-11-11 08:43:49 -0800

    Add Euler Multispecies Air (2 species) physics + sod test case

---
+++ examples/chemistry/euler_multispecies/2sp_air_sod_problem/post_process.py
@@ -0,0 +1,55 @@
+import processing.post as post
+import processing.plot as plot
+import processing.readwritedatafiles as readwritedatafiles
+
+# Read data file
+fname = "Data_final.pkl"
+solver = readwritedatafiles.read_data_file(fname)
+
+# Unpack
+mesh = solver.mesh
+physics = solver.physics
+
+# Flag for num vs avg
+num = True
+avg = False
+
+''' Plot '''
+### Density
+plot.prepare_plot()
+# Exact solution
+plot.plot_solution(mesh, physics, solver, "Density", plot_numerical=False, 
+		plot_exact=True, plot_IC=False, create_new_figure=True, 
+		fmt='k-.', legend_label="Exact")
+# DG solution
+plot.plot_solution(mesh, physics, solver, "Density", plot_numerical=num, 
+		plot_exact=False, plot_IC=False, plot_average=avg,
+		create_new_figure=False, fmt='b', legend_label="Numerical")
+
+plot.save_figure(file_name='Density', file_type='png')
+
+### Pressure
+# Exact solution
+plot.plot_solution(mesh, physics, solver, "Pressure", plot_numerical=False, 
+		plot_exact=True, plot_IC=False, create_new_figure=True, 
+		fmt='k-.', legend_label="Exact")
+# DG solution
+plot.plot_solution(mesh, physics, solver, "Pressure", plot_numerical=num, 
+		plot_exact=False, plot_IC=False, plot_average=avg,
+		create_new_figure=False, fmt='b', legend_label="Numerical")
+
+plot.save_figure(file_name='Pressure', file_type='png')
+
+### Velocity 
+# Exact solution
+plot.plot_solution(mesh, physics, solver, "Velocity", plot_numerical=False, 
+		plot_exact=True, plot_IC=False, create_new_figure=True, 
+		fmt='k-.', legend_label="Exact")
+# DG solution
+plot.plot_solution(mesh, physics, solver, "Velocity", plot_numerical=num, 
+		plot_exact=False, plot_IC=False, plot_average=avg,
+		create_new_figure=False, fmt='b', legend_label="Numerical")
+
+plot.save_figure(file_name='Velocity', file_type='png')
+
+plot.show_plot()
