46ac6591e948f305414e244fdece12e4c3ec78f0
bbornhoft <bbornhoft@gmail.com>
2021-12-17 13:45:33 -0800

    rename optional thermo to optional multispecies to be broader

--- src/external/optional_multispecies.py
+++ src/external/optional_multispecies.py
@@ -11,7 +11,7 @@ from os.path import exists
 file_exists = exists(general.cantera_lib)
 
 if file_exists:
-	import physics.chemistry.euler_multispecies.tools as thermo_tools
+	import physics.chemistry.euler_multispecies.tools as multispecies_tools
 else:
 	print("WARNING: NO EXTERNAL THERMO LIBRARY DETECTED")
-	import physics.chemistry.euler_multispecies.default_tools as thermo_tools
+	import physics.chemistry.euler_multispecies.default_tools as multispecies_tools
