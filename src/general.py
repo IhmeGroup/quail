4b30ebbde6422e345094e5c5bed3988a8dd2e8ea
balak031 <balak031@umn.edu>
2022-02-16 23:34:47 -0800

    edits

--- src/general.py
+++ src/general.py
@@ -177,6 +177,8 @@ class PhysicsType(Enum):
 		# Model for 2nd order pendulum ODE (0D model)
 	MultispeciesPSR = auto()
 		# Model for PSR (with H2/Air chemistry)
+	Wildfire = auto()
+		# Model for Wildfire Spread (2D)
 		
 		
 class TransportType(Enum):
