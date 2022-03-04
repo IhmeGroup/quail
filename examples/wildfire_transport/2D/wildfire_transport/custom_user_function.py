0aeb97bff532c5fb5fb5e49113e0955366bda1b0
balak031 <balak031@umn.edu>
2022-03-03 17:49:57 -0800

    new transport model files

---
+++ examples/wildfire_transport/2D/wildfire_transport/custom_user_function.py
@@ -0,0 +1,38 @@
+import numerics.helpers.helpers as helpers
+import pickle
+import numpy as np
+
+def custom_user_function(solver):
+	'''
+	Provides a custom interface for the model problem into the solver.
+	This particular case saves the physical time and solution in time for 
+	the "1D" element. The file written is called 'time_hist.txt'.
+
+	Inputs:
+	-------
+		solver: solver object
+	'''
+	tstart = 0.
+	if solver.time>=tstart:
+		# Unpack
+		Uc = solver.state_coeffs
+		basis_val = solver.elem_helpers.basis_val
+		Uq = helpers.evaluate_state(Uc, basis_val)
+		time_hist = open('state_variables_time_hist_TsSource_Tgas_FwoodTerm.txt', 'a') # Opens a file for appending, creates the file if it does not exist
+		
+		# Convert applicable variables to strings
+		s = str(solver.time)
+		s1 = str(Uq[0, 0, 0]) # rho_wood 
+		s2 = str(Uq[0, 0, 1]) # rho_water
+		s3 = str(Uq[0, 0, 2]) # Temperature 
+		# breakpoint()
+		# write to the file at each iteration
+		time_hist.write(s)
+		time_hist.write(' , ')
+		time_hist.write(s1)
+		time_hist.write(' , ')
+		time_hist.write(s2)
+		time_hist.write(' , ')
+		time_hist.write(s3)
+		time_hist.write('\n')
+		time_hist.close()
---
+++ examples/wildfire_transport/2D/wildfire_transport/wildfire_transport.py
@@ -0,0 +1,63 @@
+TimeStepping = {
+	"FinalTime" : 10,
+	"TimeStepSize" : 0.05,
+	"TimeStepper" : "LSRK4",
+}
+
+Numerics = {
+	"SolutionOrder" : 2,
+	"SolutionBasis" : "LagrangeQuad",
+	"ConvFluxSwitch" : True,
+}
+
+nelem = 2; 
+
+Mesh = {
+	"ElementShape" : "Quadrilateral",
+	"NumElemsX" : nelem,
+	"NumElemsY" : nelem,
+	"xmin" : -1.,
+	"xmax" : 1.,
+	"ymin" : -1.,
+	"ymax" : 1.,
+	"PeriodicBoundariesX" : ["x2", "x1"],
+	"PeriodicBoundariesY" : ["y2", "y1"], 
+}
+
+Physics = {
+	"Type" : "Wildfire",
+	"ConvFluxNumerical" : "LaxFriedrichs",
+	"Transport" : "Constant",
+	"Viscosity" : 1.81*(1e-5),
+	"PrandtlNumber" :  0.71, 
+	"SpecificHeatRatio" : 1.4,
+	"GasConstant" : 287, 
+}
+
+InitialCondition = {
+	"Function" : "WildfireBurn",
+}
+
+# ExactSolution = InitialCondition.copy()
+
+# d = {
+# 		"BCType" : "StateAll",
+# 		"Function" : "IsentropicVortex",
+# }
+
+# BoundaryConditions = {
+# 	"y1" : d,
+# 	"y2" : d,
+# }
+
+SourceTerms = {
+	"Wildfire Source Terms" : {
+		"Function" : "WildfireSource",  
+	},
+
+}
+
+Output = {
+	"AutoPostProcess" : False,
+	"Verbose" : True,
+}
